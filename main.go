9241cdde3c07f248d9ee8b06700e8ab0e78952f0
Diego Luisi <contato@diegoluisi.eti.br>
2023-11-01 16:39:34 +0000

    :telescope: Add OpenTelemetry

--- main.go
+++ main.go
@@ -1,20 +1,87 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
 	_ "github.com/devxp-tech/demo-app/config"
 	"github.com/devxp-tech/demo-app/controllers"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"google.golang.org/grpc/credentials"
+
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
+// var serviceName string
+var (
+	serviceName  = os.Getenv("SERVICE_NAME")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	insecure     = os.Getenv("INSECURE_MODE")
+)
+
+func initTracer() func(context.Context) error {
+
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if len(insecure) > 0 {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Printf("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
+
 func main() {
-	server := gin.New()
-	server.GET("/", func(c *gin.Context) {
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+	// Server
+	log.Println("Starting server...")
+	router := gin.New()
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(router)
+	router.Use(otelgin.Middleware(serviceName))
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status":  true,
 			"message": "Hello world for your app demo-app",
 		})
 	})
-	server.GET("/health-check/liveness", controllers.HealthCheckLiveness)
-	server.GET("/health-check/readiness", controllers.HealthCheckReadiness)
-	server.Run()
+	router.GET("/health-check/liveness", controllers.HealthCheckLiveness)
+	router.GET("/health-check/readiness", controllers.HealthCheckReadiness)
+	router.Run()
 }
