b307d9349f36205d4c618c3b493ad8b6a07e4fa6
Diego Luisi <contato@diegoluisi.eti.br>
2023-11-17 09:58:40 +0000

    :hammer: Remove Kustomize

--- main.go
+++ main.go
@@ -8,16 +8,19 @@ import (
 
 	"github.com/devxp-tech/demo-app/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+
 	"google.golang.org/grpc/credentials"
 
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
 var (
@@ -27,6 +30,7 @@ var (
 )
 
 func initTracer() func(context.Context) error {
+	log.Print("Initializing OpenTelemetry")
 
 	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
 	if len(insecure) > 0 {
@@ -65,17 +69,35 @@ func initTracer() func(context.Context) error {
 	return exporter.Shutdown
 }
 
+func LogrusFields(span oteltrace.Span) logrus.Fields {
+	return logrus.Fields{
+		"span_id":  span.SpanContext().SpanID().String(),
+		"trace_id": span.SpanContext().TraceID().String(),
+	}
+}
+
 func main() {
+	// Ensure logrus behaves like TTY is disabled
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableColors: true,
+		FullTimestamp: true,
+	})
 	// Server
 	cleanup := initTracer()
 	defer cleanup(context.Background())
 	fmt.Printf("Starting Appplication %v.. \n", serviceName)
 	fmt.Printf("Sending traces to: %v \n", collectorURL)
+
+	initTracer()
 	router := gin.New()
 	p := ginprometheus.NewPrometheus("gin")
 	p.Use(router)
 	router.Use(otelgin.Middleware(serviceName))
 	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status":  true,
+			"message": "Hello world for your app " + serviceName,
+		})
 	})
 	router.GET("/health-check/liveness", controllers.HealthCheckLiveness)
 	router.GET("/health-check/readiness", controllers.HealthCheckReadiness)
